53edd859b29d7e9012362800ac0a735011bde69f
sdthaker <71615163+sdthaker@users.noreply.github.com>
2020-12-12 16:22:22 -0500

    Add files via upload

---
+++ Assignments/Assignment 2/contactHelpers.c
@@ -0,0 +1,60 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     2
+// Milestone:      2
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+// This source file needs to "know about" the functions you prototyped
+// in the contact helper header file.
+// HINT: Put the header file name in double quotes so the compiler knows
+//       to look for it in the same directory/folder as this source file
+//       #include your contactHelpers header file on the next line:
+
+
+//--------------------------------
+// Function Definitions
+//--------------------------------
+
+// +-------------------------------------------------+
+// | ====== Assignment 2 | Milestone 2 =======       |
+// +-------------------------------------------------+
+// | NOTE:  Copy/Paste your Assignment-2 Milestone-1 |
+// |        empty function definitions below and     |
+// |        complete the definitions as per the      |
+// |        Milestone-2 specifications               | 
+// |                                                 |
+// | - The clearKeyboard function is done for you    |
+// +-------------------------------------------------+
+
+
+// Clear the standard input buffer
+void clearKeyboard(void)
+{
+    while (getchar() != '\n')   ; // empty execution code block on purpose
+}
+
+// pause function definition goes here:
+
+
+// getInt function definition goes here:
+
+
+// getIntInRange function definition goes here:
+
+
+// yes function definition goes here:
+
+
+// menu function definition goes here:
+
+
+// contactManagerSystem function definition goes here:
---
+++ Assignments/Assignment 2/contacts.c
@@ -0,0 +1,19 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     2
+// Milestone:      2
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// +-------------------------------------------------+
+// | NOTE:  Copy/Paste your Assignment-2 Milestone-1 |
+// |        function definitions below...            |
+// |        complete the definitions as per the      |
+// |        Milestone-2 specifications               | 
+// +-------------------------------------------------+
